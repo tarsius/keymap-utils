03cf80110889dce5e41b3972ef841c3ac1e02105
Jonas Bernoulli <jonas@bernoul.li>
2009-11-16 23:50:57 +0100

    remove reference to wid-keymap, indent commentary correctly

--- keymap-utils.el
+++ keymap-utils.el
@@ -5,7 +5,7 @@
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Created: 20080830
 ;; Updated: 20091116
-;; Version: 0.1.1+
+;; Version: 0.2
 ;; Homepage: http://github.com/tarsius/keymap-utils
 ;; Keywords: convenience, extensions
 
@@ -26,23 +26,22 @@
 
 ;;; Commentary:
 
-;;  This library provides additional keymap predicates and other keymap
-;;  functions.  It was written for library `wid-keymap' but made available
-;;  seperatly in the hope that it can be of use elsewhere also.
+;; This library provides additional keymap predicates and other keymap
+;; functions.
 
-;;  This library assumes that the char-table of a full keymap is to be
-;;  found in its cadr.  This actually does not have to be the case but
-;;  it is for keymaps that have been created using the standart mechanisms
-;;  for creating and modifying keymaps.  Key lookup in a full keymap that
-;;  has other bindings before the char-table still works as expected, but
-;;  since it is a common assumption that the char-table is in the cadr
-;;  (the doc-string of `make-keymap' states that full keymaps have the
-;;  form (keymap CHARTABLE . ALIST) ), it is legimate that I make this
-;;  assumtion here also.  If your library has problems because of this
-;;  then it probably should be fixed; not `keymap-utils'.
+;; This library assumes that the char-table of a full keymap is to be
+;; found in its cadr.  This actually does not have to be the case but
+;; it is for keymaps that have been created using the standart mechanism
+;; for creating and modifying keymaps.  Key lookup in a full keymap that
+;; has other bindings before the char-table still works as expected, but
+;; since it is a common assumption that the char-table is in the cadr
+;; (the doc-string of `make-keymap' states that full keymaps have the
+;; form (keymap CHARTABLE . ALIST) ), it is legimate that I make this
+;; assumtion here also.  If your library has problems because of this
+;; then it probably should be fixed; not `keymap-utils'.
 
-;;  This library is not intended to be used with menus, before using a
-;;  function with a menu make sure it still behaves as you intended.
+;; This library is not intended to be used with menus, before using a
+;; function with a menu make sure it still behaves as you intended.
 
 ;;; Code:
 
