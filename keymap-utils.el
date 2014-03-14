78ba657542030b608764e95f88534cbdd47198e1
Jonas Bernoulli <jonas@bernoul.li>
2014-03-14 17:28:28 +0100

    remove dependency on naked.el

--- keymap-utils.el
+++ keymap-utils.el
@@ -32,7 +32,6 @@
 ;;; Code:
 
 (require 'cl-lib)
-(require 'naked)
 
 (declare-function save-sexp-save-generic "save-sexp")
 (declare-function save-sexp-delete "save-sexp")
@@ -241,7 +240,8 @@ For an approximate inverse of this, see `kmu-parse-key-description'."
                        (concat (kmu-key-description prefix) " "))
                   (kmu-key-description (vector (car keys))) ".."
                   (kmu-key-description (vector (cdr keys)))))
-      (let ((s (naked-key-description keys)))
+      (let ((s (replace-regexp-in-string "<\\([^>]+\\)>" "\\1"
+                                         (key-description keys prefix) t)))
         ;; Merge ESC into following event.  FIXME is this still required?
         (while (and (string-match "\\(ESC \\([ACHsS]-\\)*\\([^ ]+\\)\\)" s)
                     (not (string-match-p "\\(ESC\\|M-\\)" (match-string 3 s))))
@@ -250,11 +250,113 @@ For an approximate inverse of this, see `kmu-parse-key-description'."
 
 (defun kmu-parse-key-description (string &optional need-vector)
   (if (string-match "\\.\\." string)
-      (cons (naked-edmacro-parse-keys
-             (substring string 0 (match-beginning 0)) need-vector)
-            (naked-edmacro-parse-keys
-             (substring string (match-end 0)) need-vector))
-    (naked-edmacro-parse-keys string need-vector)))
+      (cons (save-match-data
+              (kmu-parse-key-description
+               (substring string 0 (match-beginning 0)) need-vector))
+            (save-match-data
+              (kmu-parse-key-description
+               (substring string (match-end 0)) need-vector)))
+    (let ((case-fold-search nil)
+          (len (length string)) ; We won't alter string in the loop below.
+          (pos 0)
+          (res []))
+      (while (and (< pos len)
+                  (string-match "[^ \t\n\f]+" string pos))
+        (let* ((word-beg (match-beginning 0))
+               (word-end (match-end 0))
+               (word (substring string word-beg len))
+               (times 1)
+               key)
+          ;; Try to catch events of the form "<as df>".
+          (if (string-match "\\`<[^ <>\t\n\f][^>\t\n\f]*>" word)
+              (setq word (match-string 0 word)
+                    pos (+ word-beg (match-end 0)))
+            (setq word (substring string word-beg word-end)
+                  pos word-end))
+          (when (string-match "\\([0-9]+\\)\\*." word)
+            (setq times (string-to-number (substring word 0 (match-end 1))))
+            (setq word (substring word (1+ (match-end 1)))))
+          (cond ((string-match "^<<.+>>$" word)
+                 (setq key (vconcat (if (eq (key-binding [?\M-x])
+                                            'execute-extended-command)
+                                        [?\M-x]
+                                      (or (car (where-is-internal
+                                                'execute-extended-command))
+                                          [?\M-x]))
+                                    (substring word 2 -2) "\r")))
+                ((or (equal word "REM") (string-match "^;;" word))
+                 (setq pos (string-match "$" string pos)))
+                ((and (or (string-match "^\\(\\([ACHMsS]-\\)*\\)<\\(..+\\)>$" word)
+                          (and (string-match
+                                "^\\(\\([ACHMsS]-\\)*\\)\\([^ \t\f\n][^ \t\f\n]+\\)$"
+                                word)
+                               (not (string-match-p "\\([ACHMsS]-.\\)+$" word))))
+                      (progn
+                        (setq word (concat (substring word (match-beginning 1)
+                                                      (match-end 1))
+                                           (substring word (match-beginning 3)
+                                                      (match-end 3))))
+                        (not (string-match
+                              "\\<\\(NUL\\|RET\\|LFD\\|ESC\\|SPC\\|DEL\\|TAB\\)$"
+                              word))))
+                 (setq key (list (intern word))))
+                (t
+                 (let ((orig-word word) (prefix 0) (bits 0))
+                   (while (string-match "^[ACHMsS]-." word)
+                     (cl-incf bits (cdr (assq (aref word 0)
+                                              '((?A . ?\A-\^@) (?C . ?\C-\^@)
+                                                (?H . ?\H-\^@) (?M . ?\M-\^@)
+                                                (?s . ?\s-\^@) (?S . ?\S-\^@)))))
+                     (cl-incf prefix 2)
+                     (cl-callf substring word 2))
+                   (when (string-match "^\\^.$" word)
+                     (cl-incf bits ?\C-\^@)
+                     (cl-incf prefix)
+                     (cl-callf substring word 1))
+                   (let ((found (assoc word '(("NUL" . "\0") ("RET" . "\r")
+                                              ("LFD" . "\n") ("TAB" . "\t")
+                                              ("ESC" . "\e") ("SPC" . " ")
+                                              ("DEL" . "\177")))))
+                     (when found (setq word (cdr found))))
+                   (when (string-match "^\\\\[0-7]+$" word)
+                     (cl-loop for ch across word
+                              for n = 0 then (+ (* n 8) ch -48)
+                              finally do (setq word (vector n))))
+                   (cond ((= bits 0)
+                          (setq key word))
+                         ((and (= bits ?\M-\^@) (stringp word)
+                               (string-match "^-?[0-9]+$" word))
+                          (setq key (cl-loop for x across word
+                                             collect (+ x bits))))
+                         ((/= (length word) 1)
+                          (error "%s must prefix a single character, not %s"
+                                 (substring orig-word 0 prefix) word))
+                         ((and (/= (logand bits ?\C-\^@) 0) (stringp word)
+                               ;; We used to accept . and ? here,
+                               ;; but . is simply wrong,
+                               ;; and C-? is not used (we use DEL instead).
+                               (string-match "[@-_a-z]" word))
+                          (setq key (list (+ bits (- ?\C-\^@)
+                                             (logand (aref word 0) 31)))))
+                         (t
+                          (setq key (list (+ bits (aref word 0)))))))))
+          (when key
+            (cl-loop repeat times do (cl-callf vconcat res key)))))
+      (when (and (>= (length res) 4)
+                 (eq (aref res 0) ?\C-x)
+                 (eq (aref res 1) ?\()
+                 (eq (aref res (- (length res) 2)) ?\C-x)
+                 (eq (aref res (- (length res) 1)) ?\)))
+        (setq res (cl-subseq res 2 -2)))
+      (if (and (not need-vector)
+               (cl-loop for ch across res
+                        always (and (characterp ch)
+                                    (let ((ch2 (logand ch (lognot ?\M-\^@))))
+                                      (and (>= ch2 0) (<= ch2 127))))))
+          (concat (cl-loop for ch across res
+                           collect (if (= (logand ch ?\M-\^@) 0)
+                                       ch (+ ch 128))))
+        res))))
 
 ;;; Defining Bindings.
 
@@ -264,7 +366,7 @@ This is like `define-key' but if KEY is a string then it has to
 be a key description as returned by `key-description' and not a
 string like \"?\C-a\"."
   (define-key keymap
-    (if (stringp key) (naked-edmacro-parse-keys key t) key)
+    (if (stringp key) (kmu-parse-key-description key t) key)
     def))
 
 (defun kmu-remove-key (keymap key)
@@ -295,7 +397,7 @@ Note that in a full keymap all characters without modifiers are
 always bound to something, the closest these events can get to
 being undefined is being bound to nil like B above."
   (when (stringp key)
-    (setq key (naked-edmacro-parse-keys key t)))
+    (setq key (kmu-parse-key-description key t)))
   (define-key keymap key nil)
   (setq key (cl-mapcan (lambda (k)
                          (if (and (integerp k)
