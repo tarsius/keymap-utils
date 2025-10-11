d0e5672aba7f3482414c1a87003de3d87b2cab5c
Jonas Bernoulli <jonas@bernoul.li>
2025-10-11 20:22:44 +0200

    Release version 4.1.0

--- keymap-utils.el
+++ keymap-utils.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/tarsius/keymap-utils
 ;; Keywords: convenience extensions
 
-;; Package-Version: 4.0.3
+;; Package-Version: 4.1.0
 ;; Package-Requires: (
 ;;     (emacs  "28.1")
 ;;     (compat "30.1"))
